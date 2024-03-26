a7b7c2ffb4bcc39e50d9bb655ccf7d30e34d6cf7
mungsil <thddmd2009@gmail.com>
2024-03-26 17:28:34 +0900

    [chore] 프로젝트 초기 구조 설정 및 버전 동기화

---
+++ src/main/java/kyonggi/bookslyserver/BookslyServerApplication.java
@@ -0,0 +1,13 @@
+package kyonggi.bookslyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookslyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookslyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyonggi/bookslyserver/BookslyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kyonggi.bookslyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookslyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
