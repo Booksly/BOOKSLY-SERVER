513b564ab85bf24fcc8f08b5aea893ecf37a29f7
mungsil <thddmd2009@gmail.com>
2024-03-31 01:30:57 +0900

    [feat] #4 OAuth 로그인 성공 이후 액세스 토큰 발급 및 임시 리다이렉트

---
+++ src/main/java/kyonggi/bookslyserver/global/auth/handelr/OAuthAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package kyonggi.bookslyserver.global.auth.handelr;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kyonggi.bookslyserver.global.auth.jwt.JwtProvider;
+import kyonggi.bookslyserver.global.auth.principal.PrincipalDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuthAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+
+    private final JwtProvider jwtProvider;
+    private String Authorization ="Authorization";
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        String socialId = principal.getSocialId();
+        String accessToken = jwtProvider.createAccessToken(socialId);
+        response.addHeader(Authorization, accessToken);
+        log.info(accessToken);
+        getRedirectStrategy().sendRedirect(request, response, "http://localhost:8080/api/user/test");
+    }
+}
