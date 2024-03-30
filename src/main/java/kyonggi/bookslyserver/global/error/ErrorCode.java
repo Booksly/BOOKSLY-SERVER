7368741b026d68b65daac659a9aabb0903843b19
mungsil <thddmd2009@gmail.com>
2024-03-31 01:50:15 +0900

    [feat] #4 인증 및 인가 예외처리에 필요한 에러코드 추가

--- src/main/java/kyonggi/bookslyserver/global/error/ErrorCode.java
+++ src/main/java/kyonggi/bookslyserver/global/error/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
     /**
      * 401 Unauthorized
      */
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요."),
     INVALID_ACCESS_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "액세스 토큰의 값이 올바르지 않습니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다. 재발급 받아주세요."),
@@ -36,6 +36,7 @@ public enum ErrorCode {
     /**
      * 404 Not Found
      */
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 회원입니다."),
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "엔티티를 찾을 수 없습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND,"요청한 리소스를 찾을 수 없습니다."),
 
