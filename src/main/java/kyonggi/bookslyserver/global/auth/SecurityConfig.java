c98cfecb545f170c763a2328bd1f3b269886f394
mungsil <thddmd2009@gmail.com>
2024-03-31 01:52:23 +0900

    [feat] #4 SecurityConfig 클래스 추가

---
+++ src/main/java/kyonggi/bookslyserver/global/auth/SecurityConfig.java
@@ -0,0 +1,91 @@
+package kyonggi.bookslyserver.global.auth;
+
+import kyonggi.bookslyserver.domain.user.repository.UserRepository;
+import kyonggi.bookslyserver.global.auth.handelr.CustomAccessDeniedHandler;
+import kyonggi.bookslyserver.global.auth.handelr.OAuthAuthenticationSuccessHandler;
+import kyonggi.bookslyserver.global.auth.jwt.ExceptionHandlerFilter;
+import kyonggi.bookslyserver.global.auth.handelr.JwtAuthenticationEntryPoint;
+import kyonggi.bookslyserver.global.auth.jwt.JwtAuthenticationFilter;
+import kyonggi.bookslyserver.global.auth.jwt.JwtProvider;
+import kyonggi.bookslyserver.global.auth.principal.PrincipalOAuth2UserService;
+import kyonggi.bookslyserver.global.config.CorsConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RememberMeConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CorsConfig corsConfig;
+    private final JwtProvider jwtProvider;
+    private final PrincipalOAuth2UserService principalOauth2UserService;
+    private final OAuthAuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+    private final UserRepository userRepository;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        /**
+         * JWT 사용 설정
+         */
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .rememberMe(RememberMeConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        /**
+         * OAuth 로그인 설정
+         */
+        http
+                .oauth2Login(LoginConfigurer -> LoginConfigurer
+                        .userInfoEndpoint(endpointConfig -> endpointConfig.userService(principalOauth2UserService))
+                        .successHandler(oAuth2AuthenticationSuccessHandler));
+
+        /**
+         * 접근 허용 uri 추가
+         */
+        http
+                .authorizeHttpRequests((authz) -> authz
+
+                        //인증이 필요한 uri - 회원만 접근 가능
+                        .requestMatchers("api/user/need-login").authenticated()
+
+                        // 인증이 필요없음 - 비회원 & 회원 접근 가능
+                        .requestMatchers( "/healthcheck","api/user/test").permitAll()
+                        .anyRequest().authenticated());
+
+        /**
+         * 에러 발생 처리
+         */
+        http
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .exceptionHandling(exceptionHandlingConfigurer->
+                        exceptionHandlingConfigurer.accessDeniedHandler(customAccessDeniedHandler));
+
+        /**
+         * 필터 추가
+         */
+        http
+                .addFilter(corsConfig.corsFilter())
+                .addFilterBefore(new JwtAuthenticationFilter(userRepository,jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
