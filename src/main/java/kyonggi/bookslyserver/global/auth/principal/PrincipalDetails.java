624e29a8f356f7dbe49b651f2a071e821dae93ca
mungsil <thddmd2009@gmail.com>
2024-03-31 01:11:05 +0900

    [feat] #4 OAuth 로그인 지원을 위한 PrincipalDetails 클래스 추가

---
+++ src/main/java/kyonggi/bookslyserver/global/auth/principal/PrincipalDetails.java
@@ -0,0 +1,43 @@
+package kyonggi.bookslyserver.global.auth.principal;
+
+import kyonggi.bookslyserver.domain.user.entity.User;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class PrincipalDetails implements OAuth2User {
+
+    private User user;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    public PrincipalDetails(User user,Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    // 해당 User의 권한을 리턴하는 곳
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add((GrantedAuthority) () -> user.getRole().toString());
+        return collect;
+    }
+
+    @Override
+    public String getName() {
+        return user.getNickname();
+    }
+
+    public String getSocialId() {
+        return user.getSocialId();
+    }
+}
