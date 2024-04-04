d14cb25e5d838531e3bfcb9e6ee8ad5c5fd3e05f
jyuny <adela0603@kyonggi.ac.kr>
2024-04-04 10:50:37 +0900

    [feat] #11 예약 도메인 엔티티 1차 세팅

---
+++ src/main/java/kyonggi/bookslyserver/domain/reservation/entity/EmployeeReservation.java
@@ -0,0 +1,36 @@
+package kyonggi.bookslyserver.domain.reservation.entity;
+
+import jakarta.persistence.*;
+import kyonggi.bookslyserver.domain.shop.entity.Employee;
+import kyonggi.bookslyserver.domain.shop.entity.Shop;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class EmployeeReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "tinyint(0) default 0")
+    private boolean isConfirmed;
+
+    @Column(columnDefinition = "tinyint(0) default 0")
+    private boolean isRefused;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="reservation_id")
+    private Reservation reservation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="shop_id")
+    private Shop shop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="employee_id")
+    private Employee employee;
+}
--- src/main/java/kyonggi/bookslyserver/domain/reservation/entity/Reservation.java
+++ src/main/java/kyonggi/bookslyserver/domain/reservation/entity/Reservation.java
@@ -5,6 +5,8 @@ import kyonggi.bookslyserver.domain.user.entity.User;
 import kyonggi.bookslyserver.global.common.BaseTimeEntity;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -16,6 +18,27 @@ public class Reservation extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime reserveDate;
+
+    @Column(nullable = false)
+    private LocalDateTime reserveTime;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column
+    private String request;
+
+    @Column(columnDefinition = "tinyint(0) default 0")
+    private boolean isCanceled;
+
+//    @Column(columnDefinition = "tinyint(0) default 0")
+//    private boolean isClosed;
+
+//    @Column(nullable = false)
+//    @Enumerated(EnumType.STRING)
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
