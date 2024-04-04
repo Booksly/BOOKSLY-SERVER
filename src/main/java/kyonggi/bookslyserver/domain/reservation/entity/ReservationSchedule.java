fba1faa2fd67362679a50b0146d4af1f95f62f6a
jyuny <adela0603@kyonggi.ac.kr>
2024-04-04 14:05:04 +0900

    [feat] #11 예약 도메인 엔티티 2차 세팅

--- src/main/java/kyonggi/bookslyserver/domain/reservation/entity/ReservationSchedule.java
+++ src/main/java/kyonggi/bookslyserver/domain/reservation/entity/ReservationSchedule.java
@@ -2,6 +2,8 @@ package kyonggi.bookslyserver.domain.reservation.entity;
 
 import jakarta.persistence.*;
 import kyonggi.bookslyserver.domain.event.entity.ClosingEvent;
+import kyonggi.bookslyserver.domain.shop.entity.Shop;
+import kyonggi.bookslyserver.domain.shop.entity.WorkDays;
 import kyonggi.bookslyserver.global.common.BaseTimeEntity;
 import lombok.*;
 
@@ -17,8 +19,27 @@ public class ReservationSchedule extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private int startTime;
+    @Column(nullable = false)
+    private int endTime;
+
+    @Column(columnDefinition = "tinyint(0) default 0")
+    private boolean isClosed;
+
+    @Column(columnDefinition = "tinyint(0) default 0")
+    private boolean isClosingEvent;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "closingEvent_id")
     private ClosingEvent closingEvent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shop_id")
+    private Shop shop;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workDays_id")
+    private WorkDays workDays;
 
 }
