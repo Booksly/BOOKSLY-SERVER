0a937434686ebf1c7cd581abb3f4f80402a8ae72
mungsil <thddmd2009@gmail.com>
2024-04-11 22:57:56 +0900

    [refactor] #10 요청 dto 검증 어노테이션 적용

--- src/main/java/kyonggi/bookslyserver/domain/user/dto/request/JoinOwnerRequestDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/request/JoinOwnerRequestDto.java
@@ -2,12 +2,13 @@ package kyonggi.bookslyserver.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import kyonggi.bookslyserver.domain.user.validation.annotation.NotFalse;
+import kyonggi.bookslyserver.domain.user.validation.annotation.VerifyPhoneNum;
 
 public record JoinOwnerRequestDto(
         @NotNull String loginId,
         @NotNull String password,
         @NotNull String businessNumber,
-        @NotNull String phoneNum,
+        @VerifyPhoneNum String phoneNum,
         @NotFalse Boolean isVerify,
         @NotNull String email
 ) { }
