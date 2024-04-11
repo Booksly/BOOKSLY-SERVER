17566cedd7dc6727ac592274ddc6317131ea292e
mungsil <thddmd2009@gmail.com>
2024-04-11 18:59:34 +0900

    [feat] #10 본인인증하지 않은 회원가입 요청의 검증 어노테이션 추가

--- src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
@@ -1,11 +1,13 @@
 package kyonggi.bookslyserver.domain.user.controller;
 
+import jakarta.validation.Valid;
 import kyonggi.bookslyserver.domain.user.dto.request.JoinOwnerRequestDto;
 import kyonggi.bookslyserver.domain.user.dto.response.JoinOwnerResponseDto;
 import kyonggi.bookslyserver.domain.user.service.ShopOwnerService;
 import kyonggi.bookslyserver.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/owner")
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class ShopOwnerController {
 
     private final ShopOwnerService shopOwnerService;
 
     @PostMapping("/")
-    public ResponseEntity<SuccessResponse<?>> join(@RequestBody JoinOwnerRequestDto joinOwnerRequestDto) {
+    public ResponseEntity<SuccessResponse<?>> join(@RequestBody @Valid JoinOwnerRequestDto joinOwnerRequestDto) {
         JoinOwnerResponseDto joinOwnerResponseDto = shopOwnerService.join(joinOwnerRequestDto);
         return SuccessResponse.created(joinOwnerResponseDto);
     }
--- src/main/java/kyonggi/bookslyserver/domain/user/dto/request/JoinOwnerRequestDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/request/JoinOwnerRequestDto.java
@@ -1,12 +1,13 @@
 package kyonggi.bookslyserver.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import kyonggi.bookslyserver.domain.user.validation.annotation.NotFalse;
 
 public record JoinOwnerRequestDto(
         @NotNull String loginId,
         @NotNull String password,
         @NotNull String businessNumber,
-        @NotNull String phoneNum,// 본인인증 검증 어노테이션 추가
+        @NotNull String phoneNum,
+        @NotFalse Boolean isVerify,
         @NotNull String email
 ) { }
