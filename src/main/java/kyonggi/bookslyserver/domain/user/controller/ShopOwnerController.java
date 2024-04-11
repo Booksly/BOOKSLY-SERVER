e145aa81e92cc667d62837da7fd70c0fce27006d
mungsil <thddmd2009@gmail.com>
2024-04-11 23:25:02 +0900

    [fix] #10 api url에 슬래시(/) 추가

--- src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/owner")
+@RequestMapping("/api/owner")
 @RestController
 @RequiredArgsConstructor
 @Validated
