444925f906b16c25522eb193b63ca97572167969
mungsil <thddmd2009@gmail.com>
2024-04-13 01:18:09 +0900

    [feat] #10 중복된 아이디는 가입 불가하므로 예외 반환

--- src/main/java/kyonggi/bookslyserver/domain/user/service/ShopOwnerService.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/service/ShopOwnerService.java
@@ -4,6 +4,9 @@ import kyonggi.bookslyserver.domain.user.dto.request.JoinOwnerRequestDto;
 import kyonggi.bookslyserver.domain.user.dto.response.JoinOwnerResponseDto;
 import kyonggi.bookslyserver.domain.user.entity.ShopOwner;
 import kyonggi.bookslyserver.domain.user.repository.ShopOwnerRepository;
+import kyonggi.bookslyserver.domain.user.repository.UserRepository;
+import kyonggi.bookslyserver.global.error.ErrorCode;
+import kyonggi.bookslyserver.global.error.exception.ConflictException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ShopOwnerService {
 
     private final ShopOwnerRepository shopOwnerRepository;
+    private final UserRepository userRepository;
 
     public JoinOwnerResponseDto join(JoinOwnerRequestDto joinOwnerRequestDto) {
+        String loginId = joinOwnerRequestDto.loginId();
+        if (userRepository.existsByLoginId(loginId)) {
+            throw new ConflictException(ErrorCode.CONFLICT);
+        }
         ShopOwner shopOwner = ShopOwner.createShopOwner(joinOwnerRequestDto);
 
         return JoinOwnerResponseDto.of(shopOwnerRepository.save(shopOwner));
