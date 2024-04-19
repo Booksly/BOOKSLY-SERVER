e2f0d860ece3aade6c546401b3a642fd5ea64dc2
Parkchihoon <qkrqod4010@naver.com>
2024-04-19 13:43:12 +0900

    [fix] #54 ShopRegisterDto 수정

--- src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/ShopRegisterDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/ShopRegisterDto.java
@@ -19,22 +19,3 @@ public class ShopRegisterDto{
     }
 }
 
-/*
-public static JoinOwnerResponseDto of(ShopOwner shopOwner) {
-    return JoinOwnerResponseDto.builder()
-            .shopOwnerId(shopOwner.getId())
-            .build();
-}
-*/
-
-
-/*
-public record JoinOwnerRequestDto(
-        @NotNull @CheckLoginIdFormat String loginId,
-        @NotNull @CheckPasswordStrength String password,
-        @NotNull String businessNumber,
-        @NotNull @VerifyPhoneNum String phoneNum,
-        @NotNull @NotFalse Boolean isVerify,
-        @NotNull String email
-) { }
-*/
