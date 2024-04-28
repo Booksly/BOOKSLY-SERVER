bbe104f80b6f605a59059f05f5b2031be8d039c9
jyuny <adela0603@kyonggi.ac.kr>
2024-04-28 22:51:45 +0900

    [feat] #87 QueryDsl 설정

---
+++ src/main/java/kyonggi/bookslyserver/domain/reservation/service/ReserveOwnerCommandService.java
@@ -0,0 +1,14 @@
+package kyonggi.bookslyserver.domain.reservation.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class ReserveOwnerCommandService {
+
+}
