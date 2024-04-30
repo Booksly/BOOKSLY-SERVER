180a58cb9da53787ac991fa7fa552c6d21037f41
mungsil <thddmd2009@gmail.com>
2024-04-30 13:29:39 +0900

    [feat] #99 이벤트 API uri 수정 및 시큐리티 관련 작업

--- src/main/java/kyonggi/bookslyserver/domain/event/controller/EventController.java
+++ src/main/java/kyonggi/bookslyserver/domain/event/controller/EventController.java
@@ -1,13 +1,18 @@
 package kyonggi.bookslyserver.domain.event.controller;
 
+import kyonggi.bookslyserver.global.auth.principal.shopOwner.OwnerId;
+import kyonggi.bookslyserver.global.common.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/event")
+@RequestMapping("/api/events")
 @Slf4j
 public class EventController {
+
 }
