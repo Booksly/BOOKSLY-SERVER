80cfb608ba45abc4c34e372f062cbfa6a60f1563
mungsil <thddmd2009@gmail.com>
2024-05-06 16:04:03 +0900

    [rename] #99 TimeEventSchedule 엔티티명 수정

---
+++ src/main/java/kyonggi/bookslyserver/domain/event/entity/timeEvent/EmployTimeEventSchedule.java
@@ -0,0 +1,44 @@
+package kyonggi.bookslyserver.domain.event.entity.timeEvent;
+
+import jakarta.persistence.*;
+import kyonggi.bookslyserver.domain.reservation.entity.ReservationSchedule;
+import kyonggi.bookslyserver.domain.shop.entity.Employee.Employee;
+import kyonggi.bookslyserver.global.common.BaseTimeEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class EmployTimeEventSchedule extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime startEventDateTime;
+
+    private LocalDateTime endEventDateTime;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @OneToMany(mappedBy = "employTimeEventSchedule")
+    @Builder.Default
+    private List<ReservationSchedule> reservationSchedules = new ArrayList<>();
+
+    //== 연관관계 편의 메서드 ==//
+    public void addEmployee(Employee employee) {
+        this.employee = employee;
+        employee.getTimeEventSchedules().add(this);
+    }
+}
