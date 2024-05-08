4602a4880a00d4d748268f26dae5e066fbb544bc
mungsil <thddmd2009@gmail.com>
2024-05-08 14:06:44 +0900

    [feat] #123 repository 메소드 추가

--- src/main/java/kyonggi/bookslyserver/domain/shop/repository/EmployeeRepository.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package kyonggi.bookslyserver.domain.shop.repository;
 import kyonggi.bookslyserver.domain.shop.entity.Employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
+    Optional<Employee> findByIdAndShopId(Long employeeId, Long shopId);
 }
