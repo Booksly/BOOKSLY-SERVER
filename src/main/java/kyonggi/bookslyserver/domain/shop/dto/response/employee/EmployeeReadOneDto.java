99b71fc4afdaa32bdf507a50aa6e93fdc330c7f1
Parkchihoon <qkrqod4010@naver.com>
2024-05-14 21:59:27 +0900

    [feat] #161 직원 단일 조회 기능(1)

---
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/employee/EmployeeReadOneDto.java
@@ -0,0 +1,33 @@
+package kyonggi.bookslyserver.domain.shop.dto.response.employee;
+
+import kyonggi.bookslyserver.domain.shop.entity.Employee.Employee;
+import kyonggi.bookslyserver.domain.shop.entity.Employee.EmployeeMenu;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class EmployeeReadOneDto {
+    private String imgUrl;
+
+    private String name;
+
+    private String description;
+
+    private List<EmployeeReadOneMenuDto> menus;
+
+    private List<EmployeeReadOneWorkScheduleDto> workSchedules;
+
+    public EmployeeReadOneDto(Employee employee, List<EmployeeMenu> employeeMenus){
+        this.imgUrl = employee.getProfileImgUri();
+
+        this.name = employee.getName();
+
+        this.description = employee.getSelfIntro();
+        if(employeeMenus != null) {
+            this.menus = employeeMenus.stream().map(employeeMenu -> new EmployeeReadOneMenuDto(employeeMenu.getMenu())).collect(Collectors.toList());
+        }
+        this.workSchedules = employee.getWorkSchedules().stream().map(workSchedule -> new EmployeeReadOneWorkScheduleDto(workSchedule)).collect(Collectors.toList());
+    }
+}
