41fd416778ae78972013a968b6574a871c681628
Parkchihoon <qkrqod4010@naver.com>
2024-05-17 01:44:42 +0900

    [feat] #171 직원 전체 조회(예약하기)기능 작성(2)

--- src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/employee/ReserveEmployeesDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/employee/ReserveEmployeesDto.java
@@ -7,10 +7,14 @@ import lombok.Data;
 public class ReserveEmployeesDto {
     private Long id;
 
+    private String profileImgUri;
+
     private String name;
 
     public ReserveEmployeesDto(Employee employee){
         this.id = employee.getId();
+        this.profileImgUri = employee.getProfileImgUri();
         this.name = employee.getName();
+
     }
 }
