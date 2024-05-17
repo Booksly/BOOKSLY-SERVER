af8f8869e113135a38a92ffd9e8f5ed8bf434912
Parkchihoon <qkrqod4010@naver.com>
2024-05-18 01:08:25 +0900

    [feat] #173 특정 직원 메뉴 리스트 조회(예약하기)(2)

--- src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/menu/ReserveMenusDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/menu/ReserveMenusDto.java
@@ -1,5 +1,8 @@
 package kyonggi.bookslyserver.domain.shop.dto.response.menu;
 
+import kyonggi.bookslyserver.domain.event.entity.closeEvent.ClosingEventMenu;
+import kyonggi.bookslyserver.domain.event.entity.timeEvent.EmployeeTimeEvent;
+import kyonggi.bookslyserver.domain.event.entity.timeEvent.TimeEventMenu;
 import kyonggi.bookslyserver.domain.shop.entity.Employee.EmployeeMenu;
 import kyonggi.bookslyserver.domain.shop.entity.Menu.Menu;
 import lombok.Data;
@@ -10,6 +13,12 @@ public class ReserveMenusDto {
 
     private Long employeeMenuId;
 
+    private Boolean isEvent;
+
+    private String eventDescribe;
+
+    private int discountRate;
+
     private String menuCategoryName;
 
     private String menuName;
@@ -17,10 +26,54 @@ public class ReserveMenusDto {
     private int price;
 
     public ReserveMenusDto(Menu menu, EmployeeMenu employeeMenu){
+        String closingEventDescribe = "";
+        String timeEventDescribe = "";
+
+        if(menu.getClosingEventMenus() != null){
+            for(ClosingEventMenu closingEventMenu : menu.getClosingEventMenus()){
+                if(closingEventMenu.getClosingEvent().getEmployee() == employeeMenu.getEmployee()){
+                    this.isEvent = true;
+                    closingEventDescribe = closingEventMenu.getClosingEvent().getEventMessage();
+                    this.discountRate = closingEventMenu.getClosingEvent().getDiscountRate();
+                    this.price = menu.getPrice() - menu.getPrice() * this.discountRate / 100;
+                }
+
+            }
+        }
+
+        if(menu.getTimeEventMenus() != null){
+            for(TimeEventMenu timeEventMenu : menu.getTimeEventMenus()){
+                for(EmployeeTimeEvent employeeTimeEvent : timeEventMenu.getTimeEvent().getEmployeeTimeEvents()){
+                    if(employeeTimeEvent.getEmployee() == employeeMenu.getEmployee()){
+                        this.isEvent = true;
+                        timeEventDescribe = employeeTimeEvent.getTimeEvent().getTitle();
+                        this.discountRate = employeeTimeEvent.getTimeEvent().getDiscountRate();
+                        this.price = menu.getPrice() - menu.getPrice() * this.discountRate / 100;
+                    }
+                }
+            }
+        }
+
+
+
+
+        if(closingEventDescribe != ""){
+            this.eventDescribe = "[ClosingEvent]" + closingEventDescribe;
+        }
+        else{
+            this.eventDescribe = "[TimeEvent]" + timeEventDescribe;
+        }
+
+        if(closingEventDescribe.equals("") && timeEventDescribe.equals("")){
+            this.eventDescribe = "이벤트 메뉴가 아닙니다!";
+            this.discountRate = 0;
+            this.price = menu.getPrice();
+            this.isEvent = false;
+        }
+
         this.menuId = menu.getId();
         this.employeeMenuId = employeeMenu.getId();
         this.menuCategoryName = menu.getMenuCategory().getName();
         this.menuName = menu.getMenuName();
-        this.price = menu.getPrice();
     }
 }
