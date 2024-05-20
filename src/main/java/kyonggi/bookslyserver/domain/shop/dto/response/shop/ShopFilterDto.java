054d9424df886e1ff17038156557fdfefa03e5d8
Parkchihoon <qkrrqod4010@naver.com>
2024-05-20 16:22:26 +0900

    [feat] #190 Top100 가게 조회 기능 작성(2)

--- src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/shop/ShopFilterDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/response/shop/ShopFilterDto.java
@@ -1,5 +1,6 @@
 package kyonggi.bookslyserver.domain.shop.dto.response.shop;
 
+import kyonggi.bookslyserver.domain.shop.constant.CategoryName;
 import kyonggi.bookslyserver.domain.shop.entity.Shop.Shop;
 import lombok.Data;
 
@@ -7,10 +8,13 @@ import lombok.Data;
 public class ShopFilterDto {
     private Long id;
     private String name;
-
+    private CategoryName categoryName;
+    private AddressDto address;
 
     public ShopFilterDto(Shop shop){
         this.id = shop.getId();
         this.name = shop.getName();
+        this.categoryName = shop.getCategory().getCategoryName();
+        this.address = new AddressDto(shop.getAddress());
     }
 }
