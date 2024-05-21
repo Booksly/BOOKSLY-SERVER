4cc8a9d728fecd08fbefee2a74d02ed7c6ea57b0
mungsil <thddmd2009@gmail.com>
2024-05-21 16:42:24 +0900

    [feat] #186 응답 DTO 수정

--- src/main/java/kyonggi/bookslyserver/domain/event/dto/response/GetTodayClosingEventResponseDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/event/dto/response/GetTodayClosingEventResponseDto.java
@@ -1,14 +1,29 @@
 package kyonggi.bookslyserver.domain.event.dto.response;
 
+import kyonggi.bookslyserver.domain.reservation.entity.ReservationSchedule;
+import kyonggi.bookslyserver.domain.review.entity.Review;
+import kyonggi.bookslyserver.domain.shop.entity.Shop.Shop;
+import lombok.Builder;
+
 import java.time.LocalTime;
 
+@Builder
 public record GetTodayClosingEventResponseDto(
         Long shopId,
         String shopName,
-        String storeImg,
+        String shopImg,
         float rating,
         LocalTime time,
         String message
 ) {
-    public static GetTodayClosingEventResponseDto of(){return null;}
+    public static GetTodayClosingEventResponseDto of(ReservationSchedule eventReservationSchedule){
+
+        Shop shop = eventReservationSchedule.getShop();
+
+        return GetTodayClosingEventResponseDto.builder()
+                .shopId(shop.getId())
+                .shopName(shop.getName())
+                .shopImg(shop.getShopImages().get(0).getImgUri())
+                .rating(shop.getRatingByReview())
+                .message(eventReservationSchedule.getClosingEvent().getEventMessage()).build();}
 }
--- src/main/java/kyonggi/bookslyserver/domain/event/dto/response/GetTodayClosingEventsResponseDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/event/dto/response/GetTodayClosingEventsResponseDto.java
@@ -1,13 +1,20 @@
 package kyonggi.bookslyserver.domain.event.dto.response;
 
+import kyonggi.bookslyserver.domain.reservation.entity.ReservationSchedule;
+import kyonggi.bookslyserver.domain.shop.entity.Shop.Shop;
 import lombok.Builder;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Builder
 public record GetTodayClosingEventsResponseDto(
         List<GetTodayClosingEventResponseDto> closingEvents
 ) {
-    public static GetTodayClosingEventResponseDto of(){
-        return null;}
+    public static GetTodayClosingEventsResponseDto of(List<ReservationSchedule> eventReservationSchedules){
+        return GetTodayClosingEventsResponseDto.builder()
+                .closingEvents(eventReservationSchedules.stream().map(GetTodayClosingEventResponseDto::of).collect(Collectors.toList()))
+                .build();
+        }
 }
