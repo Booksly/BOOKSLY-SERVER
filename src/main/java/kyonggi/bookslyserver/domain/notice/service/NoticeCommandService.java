40d01a77e7f2f09023d5be9a7f0ef7da8f5e585b
jyuny <adela0603@kyonggi.ac.kr>
2024-05-24 12:51:36 +0900

    [feat] #214 가게 주인, 사용자 예약 삭제

--- src/main/java/kyonggi/bookslyserver/domain/notice/service/NoticeCommandService.java
+++ src/main/java/kyonggi/bookslyserver/domain/notice/service/NoticeCommandService.java
@@ -2,8 +2,13 @@ package kyonggi.bookslyserver.domain.notice.service;
 
 import jakarta.transaction.Transactional;
 import kyonggi.bookslyserver.domain.notice.dto.NoticeResponseDTO;
+import kyonggi.bookslyserver.domain.notice.entity.ShopOwnerNotice;
+import kyonggi.bookslyserver.domain.notice.entity.UserNotice;
 import kyonggi.bookslyserver.domain.notice.repository.ShopOwnerNoticeRepository;
 import kyonggi.bookslyserver.domain.notice.repository.UserNoticeRepository;
+import kyonggi.bookslyserver.domain.reservation.entity.Reservation;
+import kyonggi.bookslyserver.global.error.ErrorCode;
+import kyonggi.bookslyserver.global.error.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -32,4 +37,13 @@ public class NoticeCommandService {
     public List<NoticeResponseDTO.ReservationRequestsResultDTO> getReservationRequestsNotices(Long shopId){
         return shopOwnerNoticeRepository.getReservationRequestsNotices(shopId);
     }
+    public String deleteNotices(Long noticeId,boolean isUser){
+        if (isUser) userNoticeRepository.findById(noticeId).orElseThrow(()-> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND))
+                .setDeleted(true);
+        else shopOwnerNoticeRepository.findById(noticeId).orElseThrow(()->new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND))
+                .setDeleted(true);
+
+        return "알림이 삭제되었습니다.";
+    }
+
 }
