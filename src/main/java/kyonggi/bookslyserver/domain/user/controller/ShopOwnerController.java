180ff1e5e64df67159e5cc7d2098eacf12cf5934
mungsil <thddmd2009@gmail.com>
2024-05-25 20:04:47 +0900

    [feat] #231 기업회원 상세정보 조회 API 컨트롤러 구현

--- src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/controller/ShopOwnerController.java
@@ -1,6 +1,7 @@
 package kyonggi.bookslyserver.domain.user.controller;
 
 
+import kyonggi.bookslyserver.domain.user.dto.response.GetOwnerDetailInfoResponseDto;
 import kyonggi.bookslyserver.domain.user.dto.response.GetOwnerLoginIdResponseDto;
 import kyonggi.bookslyserver.domain.user.dto.response.GetOwnerProfileResponseDto;
 import kyonggi.bookslyserver.domain.user.service.ShopOwnerService;
@@ -16,6 +17,13 @@ import org.springframework.web.bind.annotation.*;
 public class ShopOwnerController {
 
     private final ShopOwnerService shopOwnerService;
+
+    @GetMapping("/details")
+    public ResponseEntity<SuccessResponse<?>> getOwnerDetailInfo(@OwnerId Long ownerId) {
+        GetOwnerDetailInfoResponseDto getOwnerDetailInfoResponseDto = shopOwnerService.getOwnerDetailInfo(ownerId);
+        return SuccessResponse.ok(getOwnerDetailInfoResponseDto);
+    }
+
 
     @GetMapping("/details/login-id")
     public ResponseEntity<SuccessResponse<?>> getLoginId(@OwnerId Long ownerId) {
