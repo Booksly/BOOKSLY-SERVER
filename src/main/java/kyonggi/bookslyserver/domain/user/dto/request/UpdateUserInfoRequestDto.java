0c9f23183764775fc342a6bf9bc9ffbc592ca317
mungsil <thddmd2009@gmail.com>
2024-05-25 23:31:21 +0900

    [feat] #234 유저 정보 업데이트 API DTO 구현

--- src/main/java/kyonggi/bookslyserver/domain/user/dto/request/UpdateUserInfoRequestDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/request/UpdateUserInfoRequestDto.java
@@ -1,2 +1,9 @@
-package kyonggi.bookslyserver.domain.user.dto.request;public record UpdateUserInfoRequestDto() {
+package kyonggi.bookslyserver.domain.user.dto.request;
+
+import kyonggi.bookslyserver.domain.user.validation.annotation.VerifyPhoneNum;
+
+public record UpdateUserInfoRequestDto(
+        @VerifyPhoneNum String phoneNumber,
+        String nickname
+) {
 }
--- src/main/java/kyonggi/bookslyserver/domain/user/dto/response/UpdateUserInfoResponseDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/response/UpdateUserInfoResponseDto.java
@@ -1,2 +1,16 @@
-package kyonggi.bookslyserver.domain.user.dto.response;public record UpdateUserInfoResponseDto() {
+package kyonggi.bookslyserver.domain.user.dto.response;
+
+import kyonggi.bookslyserver.domain.user.entity.User;
+import lombok.Builder;
+
+@Builder
+public record UpdateUserInfoResponseDto(
+        String phoneNumber,
+        String nickname
+) {
+    public static UpdateUserInfoResponseDto of(User user) {
+        return UpdateUserInfoResponseDto.builder()
+                .phoneNumber(user.getPhoneNum())
+                .nickname(user.getNickname()).build();
+    }
 }
