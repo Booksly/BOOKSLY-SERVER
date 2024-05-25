08b4fd23cdc30763e45db793761b896b757178c7
mungsil <thddmd2009@gmail.com>
2024-05-25 20:05:27 +0900

    [feat] #231 기업회원 상세정보 조회 API DTO 구현

--- src/main/java/kyonggi/bookslyserver/domain/user/dto/response/GetOwnerDetailInfoResponseDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/response/GetOwnerDetailInfoResponseDto.java
@@ -1,2 +1,16 @@
-package kyonggi.bookslyserver.domain.user.dto.response;public record GetOwnerDetailInfoResponseDto() {
+package kyonggi.bookslyserver.domain.user.dto.response;
+
+import kyonggi.bookslyserver.domain.user.entity.ShopOwner;
+import lombok.Builder;
+
+@Builder
+public record GetOwnerDetailInfoResponseDto(
+        String phoneNumber,
+        String email
+) {
+    public static GetOwnerDetailInfoResponseDto of(ShopOwner shopOwner) {
+        return GetOwnerDetailInfoResponseDto.builder()
+                .phoneNumber(shopOwner.getUser().getPhoneNum())
+                .email(shopOwner.getUser().getEmail()).build();
+    }
 }
