a096295d76e67119f7395a017a1981aa887edd90
mungsil <thddmd2009@gmail.com>
2024-05-26 02:23:43 +0900

    [feat] #233 기업회원 정보 업데이트 API DTO 구현

--- src/main/java/kyonggi/bookslyserver/domain/user/dto/response/UpdateOwnerInfoResponseDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/user/dto/response/UpdateOwnerInfoResponseDto.java
@@ -5,13 +5,11 @@ import lombok.Builder;
 
 @Builder
 public record UpdateOwnerInfoResponseDto(
-        String password,
         String phoneNumber,
         String email
 ) {
     public static UpdateOwnerInfoResponseDto of(ShopOwner shopOwner) {
         return UpdateOwnerInfoResponseDto.builder()
-                .password(shopOwner.getPassword())
                 .phoneNumber(shopOwner.getUser().getPhoneNum())
                 .email(shopOwner.getUser().getEmail()).build();
     }
