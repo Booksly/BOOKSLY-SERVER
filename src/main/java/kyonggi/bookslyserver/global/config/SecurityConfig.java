eed9e43726a638b750bf5f93f94a499b2ca35501
mungsil <thddmd2009@gmail.com>
2024-05-28 09:15:32 +0900

    [feat] #247 리뷰 API url, 로그인 리다이렉트 url  접근 제한 설정

--- src/main/java/kyonggi/bookslyserver/global/config/SecurityConfig.java
+++ src/main/java/kyonggi/bookslyserver/global/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
          */
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/events/closing-events/today","/api/events/time-events/today").permitAll()
+                        .requestMatchers("/api/events/closing-events/today","/api/events/time-events/today","/api/users/test","/api/shops/reviews/**").permitAll()
 
                         //인증이 필요한 uri - 일반 회원만 접근 가능
                         .requestMatchers("/api/users/**","/api/auth/verify/user/**","api/reservations","api/reservations/all").hasRole("USER")
