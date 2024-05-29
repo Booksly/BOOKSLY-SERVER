f6ed37ababc320acc7c111a2566f6c850f7a8761
mungsil <thddmd2009@gmail.com>
2024-05-30 04:18:53 +0900

    [fix] #258 메뉴 생성 API repository 수정

--- src/main/java/kyonggi/bookslyserver/domain/shop/repository/MenuCategoryRepository.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/repository/MenuCategoryRepository.java
@@ -1,11 +1,14 @@
 package kyonggi.bookslyserver.domain.shop.repository;
 
 import jakarta.transaction.Transactional;
+import kyonggi.bookslyserver.domain.shop.entity.Menu.Menu;
 import kyonggi.bookslyserver.domain.shop.entity.Menu.MenuCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MenuCategoryRepository extends JpaRepository<MenuCategory, Long> {
     @Modifying
@@ -15,6 +18,9 @@ public interface MenuCategoryRepository extends JpaRepository<MenuCategory, Long
 
     @Query("select count(mc) > 0 from MenuCategory mc where mc.name = :name and mc.shop.shopOwner.id = :ownerId")
     boolean existsByNameAndShopOwner(@Param("name") String name, @Param("ownerId") Long ownerId);
+
+    @Query("select mc from MenuCategory mc where mc.name = :name and mc.shop.shopOwner.id = :ownerId")
+    Optional<MenuCategory> findByNameAndShopOwner(@Param("name") String name, @Param("ownerId") Long ownerId);
 
     MenuCategory findByName(String name);
 }
