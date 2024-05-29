8fe8a08aae0f215340094d668b76d252fd43dca2
mungsil <thddmd2009@gmail.com>
2024-05-30 04:06:30 +0900

    [fix] #258 메뉴 생성 API DTO 수정

--- src/main/java/kyonggi/bookslyserver/domain/shop/dto/request/menu/MenuCreateRequestDto.java
+++ src/main/java/kyonggi/bookslyserver/domain/shop/dto/request/menu/MenuCreateRequestDto.java
@@ -4,14 +4,13 @@ import jakarta.persistence.Lob;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 public record MenuCreateRequestDto(
         @NotNull String menuName,
         @NotNull int price,
         @NotNull @Lob String description,
         @NotNull String menuCategory,
-        @NotNull MultipartFile menuImg
+        MultipartFile menuImg
 ) {
 
 }
